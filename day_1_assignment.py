aa7122686c95c3587c1661f4885dd2840eb2e4b7
vybz26 <Vincentsunday7000@outlook.com>
2024-07-17 17:35:41 -0400

    initial commit

---
+++ day_1_assignment.py
@@ -0,0 +1,47 @@
+# Task 1: Code Correction
+
+weather = "sunny"
+
+if weather =="sunny":
+     print("wear sunglasses!")
+else:
+     print("take an umbrella")
+
+    #Task 2
+#  Your Mood Today  Create another if statement evaluating the users mood. Ask the user how they feel today using input(). If they say "happy", print "That's great to hear!", and if they say "sad", print "I hope your day gets better!". Ensure your if statement is properly indented.
+
+mood =input("how are you feeling today")
+
+if mood  == "happy":
+     print("thats great tp hear!")
+
+elif mood == "sad":
+     print("I hope your day gets better!")
+
+# 2 Python Data Types and type() Function
+
+variable_a = "Hello, World!"
+
+print(variable_a, type(variable_a))
+
+variable_b = 23
+
+print(variable_b, type(variable_b))
+
+variable_c = 3.14
+
+print(variable_c, type(variable_c))
+
+variable_d = True
+
+print(variable_d, type(variable_d))
+
+
+#   3. Arithmetic Operations in Daily Life
+
+bread = 2.75
+peanut_butter= 4.50
+jelly = 3.23
+
+
+print('bread + peanut_butter + jelly')
